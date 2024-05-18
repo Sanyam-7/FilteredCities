497f8b337219b68a1ab9c426cc1bc46b7811d490
sanyam jain <jainsanyam626@gmail.com>
2024-05-18 11:21:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import FilterCities from './Components/FilterCities'
+
+function App() {
+ 
+
+  return (
+    <>
+     <FilterCities></FilterCities>
+    </>
+  )
+}
+
+export default App
